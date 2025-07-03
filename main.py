8051fc6657d15770c734e8cd5000e39ae5be3b7d
Pavlo Kraevoy <korotinpavlo@gmail.com>
2025-07-03 09:53:28 +0200

    def

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("Hello")`
-print("Czesc")
+def percentage(arbitrary_num, desired_percentage):
+    x = (arbitrary_num / 100) * desired_percentage
+    return x
+
+arbitrary_num = int(input("Please enter your arbitrary number: "))
+desired_percentage = int(input("Please enter your desired percentage: "))
+
+x = percentage(arbitrary_num, desired_percentage)
+
+print(x)
